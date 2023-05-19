51c9b261573cde0855a7a64a2f5e7cd996488a21
dzwduan <dzwduan@gmail.com>
2023-05-19 00:28:44 -0700

    update part of c framework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # e203-simulator-scarab
+
+1. 提取scarab单核框架设计
---
+++ src/sim.h
@@ -0,0 +1,17 @@
+#ifndef __SIM_H__
+#define __SIM_H__
+
+
+
+
+
+enum operating_mode_enum {
+  SIMULATION_MODE,
+  WARMUP_MODE /* not implemented yet */
+};
+
+
+void full_sim(void);
+
+
+#endif
